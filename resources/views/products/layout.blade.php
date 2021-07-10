9c16a4412e956fe57faf3149b4ba1c36d052992c
Abdus Sobur Shihab <abdussoburshihab56@gmail.com>
2021-07-10 12:13:09 +0600

    crud-done

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products details</title><br>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+  <br>
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
